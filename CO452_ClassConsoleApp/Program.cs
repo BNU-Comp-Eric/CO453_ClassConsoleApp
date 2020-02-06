b3d528397bdaebc115f6949cc73e791f7b8b7993
Eric Man <eric.man95@gmail.com>
2020-02-06 22:48:10 +0000

    Initial commit

---
+++ CO452_ClassConsoleApp/Program.cs
@@ -0,0 +1,25 @@
+﻿using CO453_ClassConsoleApp.Unit4;
+using System;
+
+namespace CO453_ClassConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double miles = 7;
+            double feet = 7260;
+
+            Console.WriteLine("Eric's Distance Converter");
+           
+            DistanceConverter converter = new DistanceConverter();
+            feet = converter.ToFeet(miles);
+
+            Console.WriteLine("7 miles in feet = " + feet);
+
+            miles = converter.ToMiles(feet);
+
+            Console.WriteLine("Number of feet from miles " + miles);
+        }
+    }
+}
